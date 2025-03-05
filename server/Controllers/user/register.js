ba4f8044ae019e2630046da12c05ab76600a7a68
Chirag singh <chiragsingh8926@gmail.com>
2025-03-05 23:01:33 +0530

    Working on backend user data api

---
+++ server/Controllers/user/register.js
@@ -0,0 +1,58 @@
+const Usertable = require("../../Models/usertable");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+
+const register = async (req, res) => {
+
+
+try {
+  const salt = await bcrypt.genSalt(10)
+
+  const { first_name,last_name,dob, email, mobile, password } = req.body;
+
+  console.log ("user data", req.body);
+  res.send ("try again");
+} catch (error) {
+  res.send({ status: "faild", errors: errors });
+  console.log('faild',errors)
+}
+}
+
+module.exports = register;
+/* 
+
+const usertable = require("../../Models/usertable.js");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const emailconfig = require("../../middlewares/emailconfig.js");
+const secretKey = '12345678910';
+const register = async (req, res) => {
+  try {
+    const { first_name,last_name,dob, email, mobile, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10)
+    const bcrypt_password = await bcrypt.hash(password, salt)
+    const createuser = new usertable({
+      first_name,
+      last_name,
+      email,
+      dob,
+      mobile,
+      password:bcrypt_password,
+    });
+
+    const response = await createuser.save();
+    const token = jwt.sign({ id: response.id }, secretKey, { expiresIn: '1h' });
+
+    res.send({ status: "sucessful", data: response,token:token });
+  } catch (errors) {
+    res.send({ status: "faild", errors: errors });
+    console.log('faild',errors)
+  }
+};
+
+module.exports = register;
+
+
+*/
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const userlist = require('../Controllers/user/userList')
+const register = require('../Controllers/user/register')
 const router = express.Router()
 
 
 router.get('/',userlist)
-
+router.post('/register',register)
 
 module.exports = router
