1126336524586a23a813e87acbe8d6ecca7e7d56
Chirag singh <chiragsingh8926@gmail.com>
2025-03-08 01:38:12 +0530

    Login api fully working for user

--- server/Controllers/user/login.js
+++ server/Controllers/user/login.js
@@ -1,32 +1,7 @@
+// Login api for user
+
 const jwt = require("jsonwebtoken");
 const Usertable = require("../../Models/usertable.js");
-const secretKey = "12345678910";
-const bcrypt = require("bcryptjs");
-
-const login = async (req, res) => {
- 
-    try {
-      const { email, password } = req.body;
-
-    const user = await Usertable.findOne({ email });
-      res.send("this is login api");
-
-
-
-    } catch (errors) {
-      res.send({status: "failed", errors: errors})
-console.log("user login error is there", errors);
-
-    }
-  };
-
-
-module.exports = login;
-
-
-/*
-const jwt = require("jsonwebtoken");
-const usertable = require("../../Models/usertable.js");
 const secretKey = "12345678910";
 const bcrypt = require("bcryptjs");
 
@@ -34,7 +9,7 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const user = await usertable.findOne({ email });
+    const user = await Usertable.findOne({ email });
 
     if (!user) {
       return res.status(401).send({ message: "Invalid credentials" });
@@ -44,9 +19,7 @@ const login = async (req, res) => {
         return res.status(401).send({ message: "Password not Match" });
       } else {
         const token = jwt.sign({ id: user._id }, secretKey, { expiresIn: "100h" });
-        res
-          .status(200)
-          .send({
+        res.status(200).send({ // 200 means successfull "api working & data entered is correct"
             status: "successfull",
             message: "Login successful",
             token: token,
@@ -59,5 +32,3 @@ const login = async (req, res) => {
 };
 
 module.exports = login;
-
-*/
