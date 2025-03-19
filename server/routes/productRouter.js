d8879f51001e6f104b8535e85882cf74bbc9bae9
Chirag singh <chiragsingh8926@gmail.com>
2025-03-19 22:39:04 +0530

    working on product api & route

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const productlist = require('../Controllers/Product/productlist.js');
+const createproduct = require('../Controllers/Product/createproduct.js');
+const upload = require('../middlewares/image-uploader.js');
 
 
 
 
 router.get('/',productlist)
+router.post('/',upload.fields([
+    { name: 'product_image1', maxCount: 1 },
+    { name: 'product_image2', maxCount: 1 },
+    { name: 'product_image3', maxCount: 1 },
+    { name: 'product_image4', maxCount: 1 },
+  ]),createproduct)
 
 
 
