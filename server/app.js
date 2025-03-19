8a54f9eaff814e914b71861386fd95bea8c70be1
Chirag singh <chiragsingh8926@gmail.com>
2025-03-20 02:36:31 +0530

    Product Varient api done

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const port = 8000;
 const bodyParser = require('body-parser'); // Imports body-parser, a middleware that helps process incoming request bodies (like JSON or URL-encoded data).
 const cors = require("cors"); // Imports CORS (Cross-Origin Resource Sharing) middleware to handle cross-origin requests.
 const connectdb = require("./db/connection.js"); // Imports a local module (connection.js) from the db directory, likely for database connection setup.
+const variantrouter = require("./routes/variantRouter.js")
 
 
 // path for the defined routes 
@@ -20,7 +21,7 @@ const productrouter = require("./routes/productRouter.js")
 const database = "mongodb+srv://chiragsingh8926:edkPPeCNMN25t667@shoporiastore.lvefl.mongodb.net/?retryWrites=true&w=majority&appName=ShoporiaStore";
 
 
-// 
+// Models importing
 
 require("./Models/contactus");
 require("./Models/category");
@@ -32,14 +33,15 @@ require("./Models/wishlist");
 require("./Models/brand");
 require("./Models/address");
 require("./Models/order");
+require("./Models/product_variant");
 
-// defined routes over here [Parse JSON bodies] | ## controllers
+// defined routes over here [Parse JSON bodies] | ## controllers importing
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use("/api/user", userrouter);
 app.use("/api/category", categoryrouter);
 app.use("/api/product",productrouter);
-
+app.use("/api/variant", variantrouter);
 connectdb(database)
 app.listen(port, () => {console.log(`server is runing at ${port}`);});
